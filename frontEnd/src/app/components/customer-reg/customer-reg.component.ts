3f54ea4b3ae3cfca28e0659c333a13b60b7f3825
Alexey-Kleschov <iron-goga@yandex.ru>
2019-06-28 13:34:57 +0300

    27/06/19 modify customer reg

--- frontEnd/src/app/components/customer-reg/customer-reg.component.ts
+++ frontEnd/src/app/components/customer-reg/customer-reg.component.ts
@@ -19,6 +19,8 @@ export class CustomerRegComponent implements OnInit {
     email: new FormControl('', [Validators.required, Validators.pattern(/^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/)]),
     password: new FormControl('', [Validators.required, Validators.maxLength(10), Validators.minLength(2)]),
     name: new FormControl('', [Validators.required, Validators.pattern('[A-Za-zА-Яа-яЁё]+(\s+[A-Za-zА-Яа-яЁё]+)?')]),
+    cardNumber: new FormControl('', [Validators.required, Validators.maxLength(16), Validators.minLength(16)]),
+    cardCVC: new FormControl('', [Validators.required, Validators.maxLength(3), Validators.minLength(3)]),
   }
   getErrorMessageEmail(){
     return this.userInput.email.hasError('required') ? 'You must enter a value' :
@@ -33,6 +35,16 @@ export class CustomerRegComponent implements OnInit {
     return this.userInput.name.hasError('required') ? 'You must enter a value' :
     this.userInput.name.hasError('pattern') ? 'The name field should not contains numbers' :
         '';
+  }
+  getErrorCardNumber(){
+    return this.userInput.cardNumber.hasError('required') ? 'You must enter a value' :
+      this.userInput.cardNumber.hasError('minlength') ? 'The password is too short' :
+      this.userInput.cardNumber.hasError('maxlength') ? 'The password is too long' : '';
+  }
+  getErrorCardCVC(){
+    return this.userInput.cardCVC.hasError('required') ? 'Enter a value' :
+      this.userInput.cardCVC.hasError('minlength') ? 'Wrong data' :
+      this.userInput.cardCVC.hasError('maxlength') ? 'Wrong data' : '';
   }
   
 }
