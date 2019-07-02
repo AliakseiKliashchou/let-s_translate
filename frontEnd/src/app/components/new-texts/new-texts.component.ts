b6644c79933660e53205593b21f307493485b30f
Alexey-Kleschov <iron-goga@yandex.ru>
2019-07-02 15:04:12 +0300

    fuck2

--- frontEnd/src/app/components/new-texts/new-texts.component.ts
+++ frontEnd/src/app/components/new-texts/new-texts.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireStorage, AngularFireUploadTask } from '@angular/fire/storage';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { finalize, tap } from 'rxjs/operators';
+import { Angular2Txt } from 'angular2-txt/Angular2-txt';
 
 
 @Component({
@@ -7,9 +12,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-texts.component.css', '../../app.component.css' ]
 })
 export class NewTextsComponent implements OnInit {
-  
 
-  constructor() { }
+  task: AngularFireUploadTask;
+  percentage: Observable<number>;
+  snapshot: Observable<any>;
+  downloadURL: string;
+
+  constructor(private storage: AngularFireStorage, private db: AngularFirestore,) { }
 
   ngOnInit() {
     
@@ -30,6 +39,9 @@ export class NewTextsComponent implements OnInit {
   
   uploadText(text){
     console.log(text);
+    const path = `toTranslate/${Date.now()}_aaaaa.txt`;
+    const ref = this.storage.ref(path);
+    new Angular2Txt(text, 'My Report');
   }
 
 }
