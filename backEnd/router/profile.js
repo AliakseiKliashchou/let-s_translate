13ac027f7f594716c37e6dd6f8e1828d21b7a920
Ewelai <palevo.ewelai@gmail.com>
2019-07-08 17:25:57 +0300

    feat: get request for profile data for customer

--- backEnd/router/profile.js
+++ backEnd/router/profile.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const customerModel = require('../models/customer');
+const translatorModel = require('../models/translator');
 
 router.get('/customer', async(req, res) => {
-    res.json({message: 'Profile'})
-})
+  let id = req.query.id
+  try {
+    let profile = await customerModel.findOne({where: {id}}).then((customer) => {
+      let data = {
+        name: customer.name,
+        email: customer.email,
+        photo: customer.photo
+      }
+      res.json({data})
+    });
+  } catch(error) {
+    res.json({message: error})
+  }
+});
 
 module.exports = router;
