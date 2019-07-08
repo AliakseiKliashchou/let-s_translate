6559de55cc28357a2f614edd7a85c20347cde5e6
Ewelai <palevo.ewelai@gmail.com>
2019-07-08 16:46:21 +0300

    feat: add order by id

--- backEnd/router/orders.js
+++ backEnd/router/orders.js
@@ -34,13 +34,11 @@ router.get('/order', async(req, res) => {
   }
 });
 
-router.get('/one-order', async(req, res) => {
-  let id = req.query.id;
+router.get('/order/:id', async(req, res) => {
+  let id = req.params.id;
   try {
     let order = await orderModel.findOne({where: {id: id}})
-    // let order = await orderModel.findByPk(id, { raw: true }).then(() => {
-      res.json({order});
-    // })
+    res.json({order});
   } catch(error) {
     res.status(400).json({error, message: 'Can not find any order'});
   }
