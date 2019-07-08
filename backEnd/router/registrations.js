0d14b98d8f9be184caecca5f0d4c859e8587ad94
Ewelai <palevo.ewelai@gmail.com>
2019-07-08 13:20:47 +0300

    feat: add validation, field for photo, update package.json

--- backEnd/router/registrations.js
+++ backEnd/router/registrations.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
-// const valid = require('../validators/validator'); // valid.loginValidator,
+const { check, validationResult } = require('express-validator');
+const valid = require('../validators/validator');
 const translatorModel = require('../models/translator');
 const customerModel = require('../models/customer');
 
-router.post('/translator', async(req, res) => {
+router.post('/translator', valid.validationRegistration, async(req, res) => {
+  const result = validationResult(req);
+  const hasErrors = !result.isEmpty();
+
+  if(hasErrors) {
+    res.json({message: result})
+  }
+
   try {
     let translator = await translatorModel.create({
       role: req.body.role,
@@ -23,11 +31,18 @@ router.post('/translator', async(req, res) => {
       });
     });
   } catch(error) {
-    res.status(400).json({message: 'Email address already in use!'});
+    res.status(400).json({message: error});
   }
 });
 
-router.post('/customer', async(req, res) => {
+router.post('/customer', valid.validationRegistration, async(req, res) => {
+  const result = validationResult(req);
+  const hasErrors = !result.isEmpty();
+
+  if(hasErrors) {
+    res.json({message: result})
+  }
+
   try {
     let customer = await customerModel.create({
       role: req.body.role,
@@ -36,7 +51,8 @@ router.post('/customer', async(req, res) => {
       password: req.body.password,
       verify: false,
       creditCard: req.body.creditCard,
-      tariff: req.body.tariff
+      tariff: req.body.tariff,
+      photo: req.body.photo
     });
   
     bcrypt.hash(customer.password, 10).then((hash) => {
@@ -46,7 +62,7 @@ router.post('/customer', async(req, res) => {
       });
     });
   } catch(error) {
-    res.status(400).json({message: 'Email address already in use!'});
+    res.status(400).json({message: error});
   }
 });
 
--- backEnd/validators/validator.js
+++ backEnd/validators/validator.js
@@ -1,11 +1,15 @@
 const { check, validationResult } = require('express-validator');
 
-const loginValidator = () => {
-  check('email')
-    .isEmail().withMessage('Email is invalid!')
-    .isEmpty().withMessage('Email dont be empty')
-}
-
 module.exports = {
-  loginValidator
+
+  validationRegistration: [
+    check('email')
+      .isEmail().withMessage('Email is invalid!'),
+
+    check('password')
+      .isLength({ min:8 }).withMessage('Password must be at least 8 characters in length.')
+      .matches('\[0-9\]').withMessage('Password must contain at least 1 number.')
+      .matches('\[a-z\]').withMessage('Password must contain at least 1 lowercase letter.')
+      .matches('\[A-Z\]').withMessage('Password must contain at least 1 uppercase letter.')
+  ]
 }
