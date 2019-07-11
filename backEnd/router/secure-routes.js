84d829ea9e45405ca77e9308a590c75f6119e5e3
Ewelai <palevo.ewelai@gmail.com>
2019-07-11 17:22:21 +0300

    feat: create message and add id in waitlist

--- backEnd/router/secure-routes.js
+++ backEnd/router/secure-routes.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const order = require('./orders');
 const profile = require('./profile');
 const message = require('./messages');
+const waitlist = require('./waitlists');
 
 router.use('/', order);
 router.use('/profile', profile);
 router.use('/', message);
+router.use('/waitlist', waitlist);
 
 module.exports = router;
