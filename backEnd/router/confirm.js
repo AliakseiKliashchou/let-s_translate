71a034ae52a61b988f9b1f4c4a2060b34fecf4aa
Ewelai <palevo.ewelai@gmail.com>
2019-07-11 13:00:23 +0300

    feat: add confirm and photo of customer in messeges

--- backEnd/router/confirm.js
+++ backEnd/router/confirm.js
@@ -3,9 +3,16 @@ const router = express.Router();
 const customerModel = require('../models/customer');
 
 router.get('/confirm', async(req, res) => {
-  console.log(req.query)
-  res.json('OK')
-  console.log('CONFIRMED')
+  let guid = req.query.code;
+  let id = req.query.id;
+
+  let customer = await customerModel.findOne({where: {id}}).then((customer) => {
+    if(customer.guid === guid) {
+      customer.update({verify: true});
+    }
+  })
+
+  res.redirect('http://localhost:4200');
 })
 
 module.exports = router;
