64a89d4b4fa7e97e69d2a01fcbf4dfdad158d126
Alexey-Kleschov <iron-goga@yandex.ru>
2019-07-21 18:41:28 +0300

    21/07/19 create admin service

---
+++ frontEnd/src/app/_shared/service/admin/admin.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpParams} from '@angular/common/http';
+import {AuthService} from '../users/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminService {
+
+  constructor(
+    private http: HttpClient,
+    private authService: AuthService){ }
+
+  private URL = 'http://localhost:3000';
+
+  getAdminData(){
+    return this.http.get(`${this.URL}/secure/tariff/`);
+  }
+}
